7e6f6b350597fdd68e4dc6b0076108377040b262
lspir <leandrospir@gmail.com>
2014-11-30 07:15:00 -0300

    ListadoEstadistico hoteles mayorCantidadDeConsumiblesFacturados

--- ConsultaListadoEstadistico.sql
+++ ConsultaListadoEstadistico.sql
@@ -1,3 +1,4 @@
+/*Mayor cantidad reservas canceladas*/
 go
 CREATE FUNCTION SKYNET.hotelesMayorCantidadDeReservasCanceladas(@anio int,@trimestre smallint )
 RETURNS @retorno TABLE
@@ -19,8 +20,48 @@ BEGIN
 END
 go
 
+/* --------- ---*/
 drop function SKYNET.hotelesMayorCantidadDeReservasCanceladas
 
+/*----- ---*/
+
+/* hoteles con mayor consumibles facturados*/
+go
+CREATE FUNCTION SKYNET.hotelesMayorCantidadDeConsumiblesFacturados(@anio int,@trimestre smallint )
+RETURNS @retorno TABLE
+   (
+    Hotel     varchar(50),
+    CantidadDeConsumiblesFacturados   numeric(18,0)
+    )
+AS
+BEGIN
+	Insert into @retorno 
+	select top 5 h.calle+' '+convert(nvarchar(20),h.numCalle),SUM(ce.cantidad) as CantidadConsumibles
+	from SKYNET.Hoteles h,SKYNET.Reservas r,SKYNET.Estadias e,SKYNET.ConsumiblesEstadias ce
+	where r.hotel=h.idHotel and e.reserva=r.codigoReserva and ce.estadia=e.reserva and e.cantNoches is not null and
+	YEAR(DATEADD(DD, e.cantNoches, r.fechaDesde))=@anio and SKYNET.obtenerTrimestre(DATEADD(DD, e.cantNoches, r.fechaDesde))=@trimestre
+	and ce.itemFactura is not null
+	group by h.idHotel,h.nombre,h.calle,h.numCalle
+	order by 2 DESC
+	return
+     
+END
+go
+
+/* --------- ---*/
+drop function SKYNET.hotelesMayorCantidadDeConsumiblesFacturados
+
+/*----- ---*/
+
+
+
+	
+
+
+
+
+
+/* ------ funciones auxiliares-----*/
 GO
 CREATE FUNCTION SKYNET.obtenerTrimestre(@fecha datetime)
 returns smallint
@@ -33,8 +74,20 @@ return (select case when month(@fecha)<=3 then 1
 end
 go
 
+
+
+
+
+
+/* unit test(??????? */
 SELECT SKYNET.obtenerTrimestre(DATEADD(mm, -8, getdate()))
 
 
 SELECT *
 from SKYNET.hotelesMayorCantidadDeReservasCanceladas(2014,5)
+
+
+SELECT *
+from SKYNET.hotelesMayorCantidadDeConsumiblesFacturados(2013,1)
+
+
