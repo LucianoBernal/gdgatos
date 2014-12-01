6092b91ec4f27095aac65e7020d6ed2ff2868c9c
lspir <leandrospir@gmail.com>
2014-12-01 06:03:27 -0300

    Estadisticas habitaciones con mayor cantidad de dias ocupada

--- sql/ConsultaListadoEstadistico.sql
+++ sql/ConsultaListadoEstadistico.sql
@@ -81,6 +81,35 @@ drop function SKYNET.hotelesMayorCantidadDiasFueraDeServicio
 /*----- ---*/
 
 
+
+/* Habitaciones mayor cantidad de dias ocupadas*/
+go
+CREATE FUNCTION SKYNET.habitacionesMayorCantidadDeDiasOcupada(@anio int,@trimestre smallint )
+RETURNS @retorno TABLE
+   (
+    Hotel     varchar(50),
+    Habitacion numeric(18,0),
+    CantidadDeDiasOcupada  numeric(18,0)
+    )
+AS
+BEGIN
+	Insert into @retorno 
+	select top 5 h.calle+' '+convert(nvarchar(20),h.numCalle),eh.idHabitacion,SUM(e.cantNoches) as CantidadDeDiasOcupada
+	from SKYNET.Hoteles h,SKYNET.EstadiaPorHabitacion eh,SKYNET.Estadias e, SKYNET.Reservas r
+	where eh.idHotel =h.idHotel and eh.idEstadia=e.reserva and e.reserva=r.codigoReserva and
+	YEAR(DATEADD(DD, e.cantNoches, r.fechaDesde))=@anio and SKYNET.obtenerTrimestre(DATEADD(DD, e.cantNoches, r.fechaDesde))=@trimestre
+	group by h.idHotel,h.nombre,h.calle,h.numCalle,eh.idHabitacion
+	order by 3 DESC
+	return
+     
+END
+go
+
+/* --------- ---*/
+drop function SKYNET.habitacionesMayorCantidadDeDiasOcupada
+
+/*----- ---*/
+
 	
 
 
@@ -119,5 +148,7 @@ from SKYNET.hotelesMayorCantidadDeConsumiblesFacturados(2013,1)
 SELECT *
 from SKYNET.hotelesMayorCantidadDiasFueraDeServicio(2013,1)
 
+SELECT *
+from SKYNET.habitacionesMayorCantidadDeDiasOcupada(2013,1)
 
 
